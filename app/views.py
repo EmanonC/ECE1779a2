8cf2acd8bea36b2001e53213b186e05ce907adaa
zzhdsb123 <zzhdsb123@gmail.com>
2019-11-12 21:15:45 +0000

    Merge branch 'master' of http://github.com/EmanonC/ECE1779a2
    # Conflicts:
    #	app/views.py

--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ from werkzeug.exceptions import RequestEntityTooLarge
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import text_detection, model, db, app
-from datetime import timedelta
+from datetime import timedelta,datetime
 import boto3
+import requests
 
 # the following four image extensions are allowed
 app.config["allowed_img"] = ["png", "jpg", "jpeg", "fig"]
@@ -17,6 +18,9 @@ obj = key.Object('chaoshuai', 'key.txt')
 app.secret_key = obj.get()['Body'].read().decode('utf-8')
 
 db.create_all()
+
+httpcnt=0
+timestamp=datetime.now()
 
 
 def allowed_img(filename):
@@ -31,6 +35,48 @@ def allowed_img(filename):
     else:
         return False
 
+@app.before_request
+def Uploadhttpcounts():
+    def makeupload(dt,count,instanceid):
+        cloudwatch = boto3.client('cloudwatch',region_name='us-east-1')
+
+        # Put custom metrics
+        cloudwatch.put_metric_data(
+            MetricData=[
+                {
+                    'MetricName': 'httpRequestCount',
+                    'Dimensions': [
+                        {
+                            'Name': 'InstanceID',
+                            'Value': instanceid
+                        },
+                    ],
+                    # "Timestamp": '2019-11-10T22:06:00.000Z',
+                    "Timestamp": '{}-{}-{}T{}:{}:{}.000Z'.format(dt.year,
+                                                                 dt.month,
+                                                                 dt.day,
+                                                                 dt.hour,
+                                                                 dt.minute,
+                                                                 dt.second),
+                    'Unit': 'Count',
+                    'Value': count,
+                    'StorageResolution': 60,
+                },
+            ],
+            Namespace='SITE/TRAFFIC'
+        )
+    global httpcnt
+    global timestamp
+    dt=datetime.now()
+    if timestamp.minute!=dt.minute or timestamp.hour!=dt.hour:
+            print('hahahahahaha',httpcnt)
+            r = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
+            r=r.json()
+            instanceid=r.get('instanceId')
+            makeupload(dt=dt,count=httpcnt,instanceid=instanceid)
+            httpcnt=0
+            timestamp=dt
+    httpcnt+=1
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -261,6 +307,7 @@ def fullImg(img_id):
 
 @app.route('/delete/<img_id>')
 def delete(img_id):
+    # remove the image record in the database and the image itself in the s3 bucket
     if 'user' not in session:
         return redirect(url_for('index'))
     image_id = int(img_id.split('.')[0])
@@ -278,12 +325,18 @@ def api_register():
     username = request.form.get('username')
     password = request.form.get('password')
     if not isinstance(username, str) or not 2 <= len(username) <= 100:
-        return "406, invalid username!"
+        return jsonify(message="invalid username!",
+                       code=406)
+
     if not isinstance(username, str) or not 2 <= len(password) <= 100:
-        return "406, invalid password!"
+        return jsonify(message="invalid password!",
+                       code=406)
+
     dup_user = model.User.query.filter_by(username=username).first()
     if dup_user is not None:
-        return "406, username already exists!"
+        return jsonify(message="username already exists!",
+                       code=406)
+
     password = generate_password_hash(password + username)
     candidate_user = model.User(username=username, password=password)
     db.session.add(candidate_user)
@@ -305,7 +358,8 @@ def api_register():
         Body='',
         Key=username + '/' + 'processed/'
     )
-    return "201, user created!"
+    return jsonify(message="user created!",
+                   code=201)
 
 
 @app.route('/api/upload', methods=["POST", "GET"])
@@ -315,20 +369,27 @@ def api_upload():
     try:
         file = request.files['file']
     except RequestEntityTooLarge:
-        return "406, file too big!"
+        return jsonify(message="file too big!",
+                       code=406)
     if not isinstance(username, str) or not 2 <= len(username) <= 100:
-        return "406, invalid username or password!"
+        return jsonify(message="invalid username or password!",
+                       code=406)
     if not isinstance(username, str) or not 2 <= len(password) <= 100:
-        return "406, invalid username or password!"
+        return jsonify(message="invalid username or password!",
+                       code=406)
     candidate_user = model.User.query.filter_by(username=username).first()
     if candidate_user is None:
-        return "406, invalid username or password!"
+        return jsonify(message="invalid username or password!",
+                       code=406)
     if not check_password_hash(candidate_user.password, password + username):
-        return "406, invalid username or password!"
+        return jsonify(message="invalid username or password!",
+                       code=406)
     if file.filename == "":
-        return "406, Image must have a filename"
+        return jsonify(message="Image must have a filename!",
+                       code=406)
     if not allowed_img(file.filename):
-        return "406, That image extension is not allowed!"
+        return jsonify(message="That image extension is not allowed!",
+                       code=406)
     else:
         filename = secure_filename(file.filename)
         uploader = model.User.query.filter_by(username=username).first()
@@ -352,15 +413,18 @@ def api_upload():
             os.remove('app/static/original/' + name + '.' + ext)
             model.Image.query.filter_by(id=file_id).delete()
             db.session.commit()
-            return "406, not a valid file"
+            return jsonify(message="Invalid file!",
+                           code=406)
         s3 = boto3.client('s3')
         s3.upload_file(original_name, 'chaoshuai', username + '/original/' + name + '.' + ext)
         s3.upload_file(target_file, 'chaoshuai', username + '/processed/' + name + '.' + ext)
         os.remove('app/static/original/' + name + '.' + ext)
         os.remove('app/static/processed/' + name + '.' + ext)
-        return "201, upload success!"
+        return jsonify(message="Upload success!",
+                       code=201)
 
 
 @app.errorhandler(413)
 def request_entity_too_large(error):
-    return "413, File too big!"
+    return jsonify(message="File too big!",
+                   code=413)
